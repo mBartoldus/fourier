72d96442bd16db5d63a5edbec4f0a2428d0ba1b6
mBartoldus <mbartoldus@gmail.com>
2024-06-29 00:06:00 -0400

    added optional tolerance to assertions

--- src/testing/assertions.ts
+++ src/testing/assertions.ts
@@ -1,12 +1,12 @@
 import { assertAlmostEquals } from "https://deno.land/std@0.209.0/assert/mod.ts";
 import type { Curve, Coefficients } from "../types.ts";
 
-export function assertSameCurve(actual: Curve, expected: Curve) {
+export function assertSameCurve(actual: Curve, expected: Curve, tolerance = 0.01) {
     for(let i = 0; i<expected.length; i++)
-        assertAlmostEquals(actual[i], expected[i], 0.01)
+        assertAlmostEquals(actual[i], expected[i], tolerance)
 }
 
-export function assertSameCoefficients(actual: Coefficients, expected: Coefficients) {
-    assertSameCurve(actual.real, expected.real)
-    assertSameCurve(actual.imaginary, expected.imaginary)
+export function assertSameCoefficients(actual: Coefficients, expected: Coefficients, tolerance = 0.01) {
+    assertSameCurve(actual.real, expected.real, tolerance)
+    assertSameCurve(actual.imaginary, expected.imaginary, tolerance)
 }
