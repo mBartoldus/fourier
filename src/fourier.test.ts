bf87e187e74d3908781369b22f55bad142c8f441
mBartoldus <mbartoldus@gmail.com>
2024-07-07 20:49:34 -0400

    added tests for rounding

--- src/fourier.test.ts
+++ src/fourier.test.ts
@@ -26,8 +26,8 @@ Deno.test('fourier: should accurately recreate coefficients from inverseFourier'
     const sampleRate = 100
     const harmonics = 50
     const expected = coefficients.square(harmonics)
-    const inverted = inverseFourier({ ...expected, sampleRate })
-    const actual = fourier(inverted, { harmonics })
+    const curve = inverseFourier({ ...expected, sampleRate })
+    const actual = fourier(curve, { harmonics })
     assertSameCoefficients(actual, expected)
 })
 
@@ -38,11 +38,46 @@ Deno.test('fourier: should accurately recreate coefficients from random inverseF
         real: new Float32Array(harmonics),
         imaginary: new Float32Array(harmonics),
     }
-    for(let i = 0;i<sampleRate; i++) {
-        expected.real[i] = (Math.random() * 2)-1
-        expected.real[i] = (Math.random() * 2)-1
+    for (let i = 0; i < sampleRate; i++) {
+        expected.real[i] = (Math.random() * 2) - 1
+        expected.real[i] = (Math.random() * 2) - 1
     }
-    const inverted = inverseFourier({ ...expected, sampleRate })
-    const actual = fourier(inverted, { harmonics })
+    const curve = inverseFourier({ ...expected, sampleRate })
+    const actual = fourier(curve, { harmonics })
     assertSameCoefficients(actual, expected)
+})
+
+Deno.test('fourier: should round to a given precision', () => {
+    const sampleRate = 100
+    const harmonics = 50
+    const curve = inverseFourier({
+        real: [1.45, 0, 0, 0, 0],
+        imaginary: [0, -0.999999, 0, 0, 0],
+        sampleRate
+    })
+    const rondedTo1 = fourier(curve, { harmonics, roundToNearest: 1 })
+    assertSameCoefficients(rondedTo1, {
+        real: [1, 0, 0, 0, 0],
+        imaginary: [0, -1, 0, 0, 0],
+    }, 0)
+    const roundedToHalf = fourier(curve, { harmonics, roundToNearest: 0.5 })
+    assertSameCoefficients(roundedToHalf, {
+        real: [1.5, 0, 0, 0, 0],
+        imaginary: [0, -1, 0, 0, 0],
+    }, 0)
+})
+
+Deno.test('fourier: should set coefficients within a given threshold to zero', () => {
+    const sampleRate = 100
+    const harmonics = 50
+    const curve = inverseFourier({
+        real: [0.5, 0, 0, 0, 0],
+        imaginary: [0, -1, 0, 0.1, 0],
+        sampleRate
+    })
+    const actual = fourier(curve, { harmonics, roundToNearest: 0.1, threshold: 0.2 })
+    assertSameCoefficients(actual, {
+        real: [0.5, 0, 0, 0, 0],
+        imaginary: [0, -1, 0, 0, 0],
+    }, 0)
 })
